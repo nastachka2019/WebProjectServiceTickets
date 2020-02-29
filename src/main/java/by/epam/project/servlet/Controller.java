dd9700df96bb0a2edccc666fd2bba87b86aab235
Anastasia Shpakova <shpakavaa@gmail.com>
2020-02-29 19:31:19 +0300

    add correction

--- src/main/java/by/epam/project/servlet/Controller.java
+++ src/main/java/by/epam/project/servlet/Controller.java
@@ -37,48 +37,47 @@ public class Controller extends HttpServlet {
     private static final String RESPONSE = "response";
 
     @Override
-    public void init() throws ServletException {
+    public void init() {
        ConnectionPool connectionPool= ConnectionPool.INSTANCE;
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
-            processRequest(request, response);
-        } catch (CommandException e) {
-            e.printStackTrace();
-        } catch (ServiceException e) {
-            e.printStackTrace();
-        }
+        processRequest(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
-            processRequest(request, response);
-        } catch (CommandException e) {
-            e.printStackTrace();
-        } catch (ServiceException e) {
-            e.printStackTrace();
-        }
+        processRequest(request, response);
     }
 
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws CommandException, ServiceException, IOException, ServletException {
         String page;
-      CommandMap commandMap=CommandMap.getInstance();
 
-        if (request.getParameter(COMMAND) != null) {
-            String commandStr = request.getParameter(COMMAND);
-            logger.info("Request. Parameter = " + commandStr);
+        CommandMap commandMap = CommandMap.getInstance();
+        try {
+            if (request.getParameter(COMMAND) != null) {
+              logger.info("Request. Parameter = " + request.getParameter(COMMAND));
+                Command command = commandMap.receiveCommand(request.getParameter(COMMAND));
+                page = command.execute(request, response);
 
-            Command command = commandMap.receiveCommand(commandStr);
-            page = command.execute(request,response);
+            } else if (request.getAttribute(COMMAND) != null) {
+             logger.info("Request through filter. Attribute = " + request.getAttribute(COMMAND));
+                Command command = commandMap.receiveCommand((String) request.getAttribute(COMMAND));
+                page = command.execute(request, response);
 
-        }  else {
-          logger.error("Command not received");
-            request.setAttribute(ERROR, "Command not received");
-            request.setAttribute(STATUS_CODE, 404);
-            page = PathForJsp.ERROR.getUrl();
+            } else {
+              logger.error("Command not received");
+                request.setAttribute(ERROR, "Command not received");
+                request.setAttribute(STATUS_CODE, 404);
+                page = PathForJsp.ERROR.getUrl();
+            }
+        } catch (ServiceException | CommandException e) {
+         logger.error(e.getMessage(), e);
+            request.setAttribute(ERROR, e.getMessage());
+            request.setAttribute(STATUS_CODE, 500);
+            page =PathForJsp.ERROR.getUrl();
         }
+
         if (page != null) {
             if (request.getAttribute(RESPONSE) != null && (boolean) request.getAttribute(RESPONSE)) {
                 response.sendRedirect(page);
@@ -95,7 +94,7 @@ public class Controller extends HttpServlet {
         try {
             ConnectionPool.INSTANCE.destroyPool();
         } catch (InterruptedException e) {
-          logger.error(e.getMessage(), e);
+         logger.error(e.getMessage(), e);
         }
     }
 }
